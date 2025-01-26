c3dde25de06168fa71d9dfd481311c8cb26bd6fd
Emanuel <emanuelcabral26@gmail.com>
2025-01-26 20:53:37 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,278 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAM 4
+#include <limits.h>
+#define TAM 8
 
-typedef struct grafo_lista_adjacendia {
-    int valor;
-    struct grafo_lista_adjacendia *prox;
+//Numeração do tabuleiro está invertida. e2 será na verdade e7 (com base no tabuleiro do beecrowd);
 
-} Lista_adj;
+//Definindo os movimentos do cavalo
+int movimentos[8][2] = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+                        {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
 
-void inicializa_grafo (Lista_adj **grafo) {
-    for (int i=0; i<TAM; i++) {
-        grafo[i] = NULL;
-    }
+//Função de comparação para a fila de prioridade (min-heap)
+int comparar(const void *a, const void *b) {
+    const int *linhaA = a;
+    const int *linhaB = b;
+    return linhaA[0] - linhaB[0];
 }
 
-void insere_grafo (Lista_adj **grafo, int indice, int valor) {
-
-    Lista_adj *novo = malloc(sizeof(Lista_adj));
-    Lista_adj *ant = grafo[indice];
-
-    while (ant->prox != NULL) {
-        ant = ant->prox;
-    }
-
-    novo->valor = valor;
-    novo->prox = NULL;
-    ant->prox = novo;
-
+//Função que converte o caractere da posição inicial/final para coordenadas do tabuleiro
+void converter_posicao(char *pos, int *x, int *y) {
+    *x = pos[1] - '1';  //1 deve virar 0;
+    *y = pos[0] - 'a';  //a deve virar 0;
 }
 
-void print_grafo (Lista_adj **grafo) {
+//Função Dijkstra para encontrar o menor caminho
+int dijkstra(char *pos_inicial, char *pos_final, int n, int tabuleiro[n][n]) {
+    int piaux_x, piaux_y, pfaux_x, pfaux_y;
 
-    for(int i=0; i<TAM; i++) {
-        Lista_adj *atual = grafo[i];
+    //Converte as posições iniciais e finais para coordenadas
+    converter_posicao(pos_inicial, &piaux_x, &piaux_y);
+    converter_posicao(pos_final, &pfaux_x, &pfaux_y);
 
-        if(atual == NULL) {
-            printf("[%d]:\t\n", i+1);
-        } else {
-            printf("[%d]:\t", i+1);
-            atual = atual->prox;
-            while(atual != NULL) {
-                printf("%d\t", atual->valor);
-                atual = atual->prox;
+    int fila[(n*n)] [3];    //Fila para guardar a distância, a posição com base na letra (eixo x) e a posição com base no número (eixo y);
+    int fila_tam = 0;
+    int i, j;
+
+    //Inicializa a distância como infinita e a fila;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            tabuleiro[i][j] = INT_MAX;
+        }
+    }
+    //print_matriz(n, n, tabuleiro);
+
+    tabuleiro[piaux_x][piaux_y] = 0;
+    fila[fila_tam] [0] = 0;
+    fila[fila_tam] [1] = piaux_x;
+    fila[fila_tam] [2] = piaux_y;
+    fila_tam++;
+
+    while(fila_tam > 0){
+        //Ordena a fila (menor distância vem primeiro);
+        qsort(fila, fila_tam, sizeof(fila[0]), comparar);
+
+        //Pega o nó com a menor distância
+        int dist_atual = fila[0][0];
+        int x = fila[0][1];     //Posição das letras (eixo x);
+        int y = fila[0][2];     //Posição dos números (eixo y);
+
+        //Remove o primeiro elemento da fila passando o próximo valor para o anterior ([0][0] se torna [1][0]);
+        for (i = 1; i < fila_tam; i++) {
+            fila[i - 1][0] = fila[i][0];
+            fila[i - 1][1] = fila[i][1];
+            fila[i - 1][2] = fila[i][2];
+        }
+        fila_tam--;
+
+        //Se chegou ao destino, retorna a distância;
+        if (x == pfaux_x && y == pfaux_y) {
+            return dist_atual;
+        }
+
+        //Vamos explorar todos os movimentos possíveis;
+        for(i = 0; i < 8; i++){
+            //Faz o movimento para uma direção;
+            int mx = x + movimentos[i][0];  //Movimento no eixo x;
+            int my = y + movimentos[i][1];  //Movimento no eixo y;
+
+            if(mx >= 0 && mx < n && my >= 0 && my < n){   //Verifica se ainda estamos dentro do tauleiro;
+                int nova_distancia = dist_atual+1;
+                if(nova_distancia < tabuleiro[mx][my]){
+                    tabuleiro[mx][my] = nova_distancia;   //Atualiza a distância quando ela for menor;
+
+                    fila[fila_tam][0] = nova_distancia;
+                    fila[fila_tam][1] = mx;
+                    fila[fila_tam][2] = my;
+                    fila_tam++;
+                }
             }
-            printf("\n");
-        }
-
-    }
-}
-
-int grau_saida (Lista_adj **grafo, int indice) {
-    Lista_adj *ant = grafo[indice];
-    ant = ant->prox;
-    int count = 0;
-    while(ant != NULL) {
-        count++;
-        ant = ant->prox;
-    }
-    return count;
-}
-
-int grau_entrada(Lista_adj **grafo, int indice)
-{
-    Lista_adj *ant;
-    int count = 0;
-    for(int i=0; i<TAM-1; i++) {
-        ant = grafo[i];
-        ant = ant->prox;
-        while(ant != NULL) {
-            if(ant->valor == indice)
-                count++;
-            ant = ant->prox;
-        }
-    }
-    return count;
-}
-
-void verticeIsoladoDir (Lista_adj **grafo) {
-    for(int i=1; i<=TAM; i++) {
-        if((grau_entrada (grafo, i)) == 0 && (grau_saida(grafo, i-1)) == 0)
-            printf("Vertice [%d] e isolado\n", i);
-    }
-    printf("\n");
-}
-
-void verticeIsoladoNDir (Lista_adj **grafo) {
-    for(int i=1; i<=TAM; i++) {
-        if((grau_saida(grafo, i-1)) == 0)
-            printf("Vertice [%d] e isolado\n", i);
-    }
-    printf("\n");
-}
-
-void grafo_regular_Ndir (Lista_adj **grafo) {
-    int aux = grau_saida(grafo, 0);
-    int aux2 = 0;
-    for(int i=1; i<TAM; i++) {
-        if(grau_saida(grafo, i)!=aux) {
-            aux2 = i;
-            break;
-        }
-    }
-    if(grau_saida(grafo, aux2)!=aux) {
-        printf("Grafo nao e regular\n");
-    } else {
-        printf("Grafo e regular\n");
-    }
-}
-
-void grafo_regular_dir (Lista_adj **grafo) {
-    int aux = grau_saida(grafo, 0);
-    int aux2 = grau_entrada(grafo, 0);
-    int aux3 = 0;
-    for(int i=1; i<TAM; i++) {
-        if((grau_saida(grafo, i)!=aux) || (grau_entrada(grafo, i)!=aux2)!=aux2) {
-            aux3 = i;
-            break;
-        }
-    }
-    if((grau_saida(grafo, aux3)!=aux) || (grau_entrada(grafo, aux3)!=aux2)!=aux) {
-        printf("Grafo nao e regular\n");
-    } else {
-        printf("Grafo e regular\n");
-    }
-}
-
-void grafo_completo_Ndir (Lista_adj **grafo) {
-    Lista_adj *ant;
-    int count = 0;
-    ant = grafo[0];
-    ant = ant->prox;
-    for(int i=2; i<=TAM; i++) {
-        if(ant->valor == i)
-            count++;
-        ant = ant->prox;
-    }
-    if(count == TAM-1)
-        printf("Grafo e completo\n");
-}
-
-void printvet (int *vet, int tam)
-{
-    for (int i=0; i<(tam); i++)
-        printf("%d\t", vet[i]);
-    printf("\n\n");
-}
-
-void printvetchar (char *vet, int tam)
-{
-    for (int i=0; i<(tam); i++)
-        printf("%c\t", vet[i]);
-    printf("\n\n");
-}
-
-PrintPath(Lista_adj **grafo, int beg, int end, int ant[]) {
-    if(beg == end)
-        printf("%d ", beg+1);
-    else if (ant[end-1] == -1)
-        printf("Nao existe caminho de %d ate %d", beg+1, end);
-    else {
-        PrintPath(grafo, beg, ant[end-1], ant);
-        printf("%d ", end);
-    }
-}
-
-void BFS (Lista_adj **grafo, int beg, int end) {    //Funciona, mas tem seus problemas;
-    int fila[TAM+1];
-    int dist[TAM];
-    int ant[TAM];
-    char color[TAM];
-    int aux = 0;
-    int aux2;
-    int aux3 = 1;
-
-    for(int u=0; u<TAM; u++) {
-        color[u] = 'b';
-        dist[u] = -1;
-        ant[u] = -1;
-        fila[u] = -64;
-    }
-    fila[TAM+1] = -64;
-    color[beg] = 'c';
-    dist[beg] = 0;
-    ant[beg] = -1;
-    fila[aux] = beg;
-
-    Lista_adj *aux_grafo = grafo[beg];
-    aux_grafo = aux_grafo->prox;
-
-    //return;
-
-    while( aux < aux3) {
-        aux2 = fila[aux++];
-        //printf("aux2 = %d\n\n", aux2);
-        while(aux_grafo != NULL) {
-            if(color[aux_grafo->valor-1] == 'b') {
-                color[aux_grafo->valor-1] = 'c';
-                dist[aux_grafo->valor-1] = dist[aux2]+1;
-                ant[aux_grafo->valor-1] = aux2;
-                fila[aux3++] = aux_grafo->valor;
-            }
-            //printf("%d\n\n", aux_grafo->valor);
-            aux_grafo = aux_grafo->prox;
-        }
-        //printf("aux = %d\n\n", aux);
-        color[aux2] = 'p';
-        aux_grafo = grafo[aux-1]->prox;
-    }
-    PrintPath(grafo, beg, end, ant);
-}
-
-int main () {
-
-    FILE *file;
-    int n, a, o, t1, t2, count = 0;
-    char line[10];
-    file = fopen("G6.txt", "r");
-
-    fscanf(file, "%d %d %d", &n, &a, &o);
-
-    Lista_adj *grafo [TAM];
-    for (int i = 0; i < TAM; i++) {
-        grafo[i] = malloc(sizeof(Lista_adj));
-        grafo[i]->valor = i;
-        grafo[i]->prox = NULL;
-    }
-
-    if(o == 0) {
-        while (fgets(line, sizeof(line), file)) {
-            if (count == 0) {
-                count++;
-                continue;
-            }
-            sscanf(line, "%d %d", &t1, &t2);
-            insere_grafo(grafo, t1-1, t2);
-            insere_grafo(grafo, t2-1, t1);
-        }
-    } else {
-        while (fgets(line, sizeof(line), file)) {
-            if (count == 0) {
-                count++;
-                continue;
-            }
-            sscanf(line, "%d %d", &t1, &t2);
-            insere_grafo(grafo, t1-1, t2);
         }
     }
 
-    fclose(file);
+    return -1;  //Retorna -1 se não houver caminho
+}
 
-    print_grafo(grafo);
+int main() {
+    char pos_inicial[3], pos_final[3];
+    int tabuleiro[TAM][TAM];
 
-
-    printf("\n\n");
-    int indice = 4;
-    if(o==0) {
-        printf("Grau indice de saida %d: %d\n", indice, grau_saida(grafo, indice-1));
-        verticeIsoladoNDir(grafo);
-        grafo_regular_Ndir(grafo);
-        //grafo_completo_Ndir(grafo);
-    } else {
-        printf("Grau indice de saida %d: %d\n", indice, grau_saida(grafo, indice-1));
-        printf("Grau indice de entrada %d: %d\n", indice, grau_entrada(grafo, indice));
-        verticeIsoladoDir(grafo);
-        grafo_regular_dir(grafo);
+    // Lê casos até o final do arquivo (EOF)
+    while (scanf("%s %s", pos_inicial, pos_final) != EOF) {
+        int caminho_minimo = dijkstra(pos_inicial, pos_final, TAM, tabuleiro);
+        printf("To get from %s to %s takes %d knight moves.\n", pos_inicial, pos_final, caminho_minimo);
     }
-
-
-    BFS(grafo, indice-3, 4);
-    printf("\n");
-    //remove_no(grafo, indice-1);
-    //printf("\n\n");
-    //print_grafo(grafo);
-
 
     return 0;
 }
